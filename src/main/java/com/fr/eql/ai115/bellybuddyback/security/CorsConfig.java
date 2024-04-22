dd8b9d27809a37b8562efcfa4faa385fbb51da5e
cynthiarktn <cynthiarktn@gmail.com>
2024-04-22 10:53:00 +0200

    CorsConfig.java added for registration

---
+++ src/main/java/com/fr/eql/ai115/bellybuddyback/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.fr.eql.ai115.bellybuddyback.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:3000"); // or you can put "*" to allow all origins
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
+
